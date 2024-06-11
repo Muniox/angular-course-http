7bd0dac71fa0284f4c811212e0f092d274bdb09e
Muniox <muniox20@gmail.com>
2024-06-11 13:26:30 +0200

    Sending a POST Request

---
+++ http-01/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(import.meta.env.NG_APP_URL);
+    this.http
+      .post(`${import.meta.env.NG_APP_URL}posts.json`, postData)
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
