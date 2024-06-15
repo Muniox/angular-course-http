7ef735f916988cd363124f93e671b28841f66328
Muniox <muniox20@gmail.com>
2024-06-15 13:13:54 +0200

    Response Interceptors

--- http-01/src/app/auth.interceptor.ts
+++ http-01/src/app/auth.interceptor.ts
@@ -2,11 +2,12 @@
 
 import {
   HttpEvent,
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 // export const authInterceptor: HttpInterceptorFn = (req, next) => {
 //   return next(req);
@@ -23,6 +24,13 @@ export class AuthInterceptor implements HttpInterceptor {
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz'),
     });
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap((event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Response arrived, body data:');
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
